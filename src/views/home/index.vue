bb4055ec483331b8db3db9a2d123af26e6f0b404
biyumeng <609814862@qq.com>
2019-12-19 21:07:50 +0800

    头条PC-主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>主页</div>
+    <el-container>
+        <!-- 左侧区域 -->
+        <el-aside>左侧内容</el-aside>
+        <!-- 右侧区域 -->
+        <el-container>
+            <el-header>头部</el-header>
+            <el-main>
+                主要内容
+            </el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
