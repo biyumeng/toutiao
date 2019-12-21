25beec5c75bfa49f802a0f6ce63d2fa30d3860f7
biyumeng <609814862@qq.com>
2019-12-21 20:57:04 +0800

    头条PC--接口访问-axios拦截器-统一处理响应数据

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 // 封装一个axios
 import axios from 'axios'
 
+// 请求拦截器
 axios.interceptors.request.use(function (config) {
   // 执行请求成功
   let token = window.localStorage.getItem('user-token')
@@ -8,6 +9,13 @@ axios.interceptors.request.use(function (config) {
   return config // 表示会用该config请求进行后台操作
 }, function () {
   // 执行请求失败
+})
+
+// 响应拦截器
+axios.interceptors.response.use(function (response) {
+  return response.data ? response.data : {}
+}, function () {
+
 })
 
 export default axios
