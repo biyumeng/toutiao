ba1f2db854c70e0704be3aa63ea02f6d883d22ab
biyumeng <609814862@qq.com>
2019-12-27 09:15:08 +0800

    头条PC-发表文章-频道数据加载

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -22,7 +22,9 @@
               </el-radio-group>
           </el-form-item>
           <el-form-item label="频道">
-              <el-select></el-select>
+              <el-select>
+                  <el-option v-for="item in channels" :label="item.name" :value="item.id" :key="item.id"></el-option>
+              </el-select>
           </el-form-item>
           <el-form-item>
               <el-button type="primary">发布</el-button>
@@ -34,7 +36,24 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      channels: [] // 接收频道数组
+    }
+  },
+  methods: {
+    // 获取所有的频道
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(result => {
+        this.channels = result.data.channels
+      })
+    }
+  },
+  created () {
+    this.getChannels()
+  }
 }
 </script>
 
