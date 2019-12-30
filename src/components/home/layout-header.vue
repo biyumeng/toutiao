a5529535728f20134ab3da6b4121bf8d9ede0789
biyumeng <609814862@qq.com>
2019-12-30 20:50:30 +0800

    头条PC-左侧导航栏的折叠展开

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -1,7 +1,7 @@
 <template>
     <el-row class="layout-header" type="flex" align="middle">
         <el-col :span="12" class="left">
-            <i class="el-icon-s-unfold"></i>
+            <i @click="collapseOrOpen" :class="{'el-icon-s-unfold' :collapse,'el-icon-s-fold':!collapse}"></i>
             <span>江苏传智播客教育科技股份有限公司</span>
         </el-col>
         <el-col :span="12" class="right">
@@ -26,6 +26,7 @@ import eventBus from '../../utils/eventBus'
 export default {
   data () {
     return {
+      collapse: false, // 默认是展开
       userInfo: {}, // 定义一个接受数据的对象
       defaultImg: require('../../assets/img/userlogo.jpg') // 默认图片变为动态变量再赋值
     }
@@ -39,6 +40,11 @@ export default {
     })
   },
   methods: {
+    // 点击折叠或展开
+    collapseOrOpen () {
+      this.collapse = !this.collapse // 不是展开就是折叠
+      eventBus.$emit('changeCollapse') // 触发一个事件
+    },
     // 封装一个更新用的方法
     getUserInfo () {
       // let token = localStorage.getItem('user-token')
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,8 @@
 <template>
     <el-container>
         <!-- 左侧区域 -->
-        <el-aside style="min-height:100vh;background-color:#353b4e;width: 230px;">
-            <layout-aside></layout-aside>
+        <el-aside :style="{width:collapse?'60px':'230px'}" style="min-height:100vh;background-color:#353b4e;transition:all 0.5s;">
+            <layout-aside :collapse="collapse"></layout-aside>
         </el-aside>
         <!-- 右侧区域 -->
         <el-container>
@@ -17,12 +17,20 @@
 </template>
 
 <script>
+import eventBus from '../../utils/eventBus'
 // import layoutAside from '../../components/home/layout-aside'
 // import layoutHeader from '../../components/home/layout-header'
 export default {
-  components: {
-    // 'layout-aside': layoutAside,
-    // 'layout-header': layoutHeader
+  data () {
+    return {
+      collapse: false // 默认是展开
+    }
+  },
+  created () {
+    // 开启监听
+    eventBus.$on('changeCollapse', () => {
+      this.collapse = !this.collapse // 不是展开就是折叠
+    })
   }
 }
 </script>
